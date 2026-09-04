302af197a23d178c5d8ec18d5734a57454fccc1d
Codejourney01 <141733165+Codejourney01@users.noreply.github.com>
2026-09-04 18:14:29 +0530

    testing

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -4,6 +4,7 @@ export default function App() {
   return (
     <div className='flex items-center justify-center text-red-500 text-2xl'>Edge
     <h1 className='text-2xl text-black'>hello</h1>
+    <h2 className='font-black text-2xl text-red-600'>Edge</h2>
     </div>
   )
 }
