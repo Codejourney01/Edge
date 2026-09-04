694ce53b6b87fcead3586ac5213b9d968b85564f
Vishwesh Sitapara <vishweshsitapara2007@gmail.com>
2026-09-04 18:10:49 +0530

    update

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className='flex items-center justify-center text-red-500 text-2xl'>Edge</div>
+    <div className='flex items-center justify-center text-red-500 text-2xl'>Edge
+    <h1 className='text-2xl text-black'>hello</h1>
+    </div>
   )
 }
