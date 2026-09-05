1f0222218af3ae819e922718908a9c1956292e56
Codejourney01 <141733165+Codejourney01@users.noreply.github.com>
2026-09-05 17:58:06 +0530

    hidden the desktop section

--- website/src/Page/Home.jsx
+++ website/src/Page/Home.jsx
@@ -9,7 +9,7 @@ function Home() {
     <div className="w-full h-full">
       <Navbar />
       <HeroSection />
-      <div className="flex flex-col justify-center items-center mt-20">
+      <div className="md:flex flex-col justify-center items-center mt-20 hidden">
         <Title text1={"All apps, Ai Agents and humans in Edge"} text2={"30+ Products to replace fragmented tools & maximize human productivity"}/>
         <div className="bg-[#eaeaea] w-fit flex flex-col justify-center items-center">
           <Features />
