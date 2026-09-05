cea6c305067d80d3968527b3fd95331ddc849f2c
Vishwesh Sitapara <vishweshsitapara2007@gmail.com>
2026-09-05 17:50:51 +0530

    title update

--- website/src/Page/Home.jsx
+++ website/src/Page/Home.jsx
@@ -1,14 +1,16 @@
 import Features from "@/components/Features";
 import HeroSection from "@/components/HeroSection";
 import Navbar from "@/components/Navbar";
+import Title from "@/components/Title";
 import React from "react";
 
 function Home() {
   return (
-    <div className="w-full h-full ">
+    <div className="w-full h-full">
       <Navbar />
       <HeroSection />
-      <div className="flex justify-center items-center mt-20">
+      <div className="flex flex-col justify-center items-center mt-20">
+        <Title text1={"All apps, Ai Agents and humans in Edge"} text2={"30+ Products to replace fragmented tools & maximize human productivity"}/>
         <div className="bg-[#eaeaea] w-fit flex flex-col justify-center items-center">
           <Features />
         </div>
--- website/src/components/Title.jsx
+++ website/src/components/Title.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
-function Title() {
+function Title({text1,text2}) {
   return (
-    <div>
-      <p>title</p>
+    <div className='flex flex-col items-center justify-center py-10 w-full'>
+      <p className="text-[48px] w-120 text-center font-bold bg-gradient-to-r from-black via-zinc-800 to-zinc-400 bg-clip-text text-transparent">{text1}</p>
+      <p className='text-[#888888] hidden md:flex text-[14px] md:text-[20px] font-normal mt-1'>{text2}</p>
     </div>
   )
 }
